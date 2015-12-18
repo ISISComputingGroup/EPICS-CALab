97c650b1053c27b4c54f1069fdc5617cd736a51f
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-12-18 10:20:16 +0000

    VS2015 fixes

--- caLab_1505/src/caLab.cpp
+++ caLab_1505/src/caLab.cpp
@@ -206,7 +206,7 @@ enum epicsAlarmCondition {
 
 struct cmp_str
 {
-	bool operator()(char const *a, char const *b)
+	bool operator()(char const * a, char const * b) const
 	{
 		return strcmp(a, b) < 0;
 	}
